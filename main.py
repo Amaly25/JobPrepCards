131d46cb6367c9c181c321ed7a2d1355f7c31bfd
Amal <amal.y.alshamahai@gmail.com>
2023-07-30 17:46:41 +0200

    refactoring

--- main.py
+++ main.py
@@ -22,73 +22,81 @@ def init():
 
 @st.cache_data
 def call_openai(input, _template_name):
-    llm = OpenAI(temperature=0.9)
-    chain = LLMChain(llm=llm, prompt=_template_name)
+    chain = LLMChain(llm=OpenAI(temperature=0.9), prompt=_template_name)
     response = chain.run(prompt=input)
+    return response
+
+def call_openai_multiple(input_dict, _template_name):
+    chain = LLMChain(llm=OpenAI(temperature=0.9), prompt=_template_name)
+    response = chain.run(input_dict)
     return response
 
 
 def main():
     init()
     header = st.container()
-    header.title("Job Interview Preparation..Your own knowledge Tester 🤖")
+    header.title("Job Prep Cards 🤖")
     header.subheader(
-        "Discover the ultimate job interview preparation app, designed to empower you with the skills and confidence needed to land the job of your dreams."
+        "Discover the ultimate job interview preparation app, designed "
+        "to empower you with the skills and confidence needed to land the job of your dreams."
     )
 
-    messages = [
-        SystemMessage(content=("You are a helpful assistant")),
-    ]
-
-    response = None
+    interview_question = None
     label = "Enter your topic here:"
-    prompt = header.text_input(
+    topic = header.text_input(
         label=label,
         key="user_input",
         label_visibility="hidden",
         placeholder=label,
     )
 
-    title_template = PromptTemplate(
+    topic_template = PromptTemplate(
         input_variables=["prompt"],
-        template="write a an interview question about {prompt}",
+        template=(
+            "Generate a question about a specific topic, that could be asked in a job interview."
+            " ChatGPT should not ask for personal experiences etc. The topic is: {prompt}"
+        ),
     )
 
-    if prompt:
+    if topic:
         with st.spinner("Generating a Question"):
-            response = call_openai(prompt, title_template)
-            st.write(response)
+            interview_question = call_openai(topic, topic_template)
+            st.write(interview_question)
 
-    if response:
-        label2 = "write your answer "
-        prompt1 = st.text_input(
-            label=label2,
+    if interview_question:
+        answer_label = "write your answer "
+        user_answer = st.text_input(
+            label=answer_label,
             key="user_answer",
             label_visibility="hidden",
-            placeholder=label2,
+            placeholder=answer_label,
         )
 
-        if prompt1:
+        if user_answer:
             script_template = PromptTemplate(
                 input_variables=["prompt"],
-                template="write me a appropriate answer based on this topic: {prompt} ",
+                template="What is a good summary of the correct answer to: {prompt}?",
             )
 
             compare_text_template = PromptTemplate(
-                input_variables=["prompt"],
-                template="compare the user answer in the input prompt1 based on the script_templat AI Answer and give a percentaage of the correctness of the users answer: {prompt}",
+                input_variables=["prompt1", "prompt2"],
+                template=(
+                    "Compare the user's answer to your own summary. Judge the user's answer and respond with a "
+                    "percentage that corresponds to your judgement. The user's answer was: {prompt1} and your "
+                    " own answer was: {prompt2}",
+                )
             )
 
             with st.spinner("Loding"):
-                script = call_openai(response, script_template)
-                comper_text = call_openai(prompt1, compare_text_template)
+                script = call_openai(interview_question, script_template)
+                compare_text = call_openai_multiple({'user_answer':"prompt1",'ai_answer': "prompt2"}, compare_text_template )
                 st.write(script)
-                st.write(comper_text)
+                st.write(compare_text)
 
 
 if __name__ == "__main__":
     st.set_page_config(
-        page_title="Your own knowledge Tester",
+        page_title="Job Prep Cards",
         page_icon="🤖",
         layout="centered",
         initial_sidebar_state="expanded",
