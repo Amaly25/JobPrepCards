7d3b70176cb43c0d5b5f9169256d5777843e755d
Amal <amal.y.alshamahai@gmail.com>
2023-08-02 14:59:09 +0200

    rerun button

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
-from langchain.schema import SystemMessage
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    AIMessagePromptTemplate,
+)
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
@@ -20,34 +25,40 @@ def init():
         exit(1)
     else:
         print("OPENAI_API_KEY is get")
-    return supabase    
+    return supabase
 
 
 @st.cache_data
 def call_openai(input, _template_name):
-    chain = LLMChain(llm=OpenAI(temperature=0.9), prompt=_template_name)
+    model = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0,
+    )
+    chain = LLMChain(llm=model, prompt=_template_name)
     response = chain.run(input)
     return response
 
+
 def get_data_base_data(supabase):
     table = supabase.table("jobprep")
-    print('')
-    print('')
-    print('')
+    print("")
+    print("")
+    print("")
     data = table.select("*").execute()
     return data
 
+
 @st.cache_data
 def insert_data_into_database(_supabase, input: dict):
-    table= _supabase.table("jobprep")
-    insert_data= table.insert(input).execute()
+    table = _supabase.table("jobprep")
+    insert_data = table.insert(input).execute()
     return insert_data
+
 
 def main():
     supabase = init()
     data = get_data_base_data(supabase)
     print(data)
-
 
     header = st.container()
     header.title("Job Prep Cards 🤖")
@@ -65,22 +76,20 @@ def main():
         placeholder=label,
     )
 
-    topic_template = PromptTemplate(
-        input_variables=["prompt"],
-        template=
-            "Generate a question about a specific topic, that could be asked in a job interview."
-            " ChatGPT should not ask for personal experiences etc. The topic is: {prompt}"
-       ,
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        "Generate a question about a specific topic, that could be asked in a job interview."
+        " ChatGPT should not ask for personal experiences etc. The topic is: {prompt}"
     )
+    topic_template = ChatPromptTemplate.from_messages([human_message_prompt])
 
     if topic:
         with st.spinner("Generating a Question"):
             interview_question = call_openai(topic, topic_template)
-            st.write(interview_question)
+            header.write(interview_question)
 
     if interview_question:
         answer_label = "write your answer "
-        user_answer = st.text_input(
+        user_answer = header.text_input(
             label=answer_label,
             key="user_answer",
             label_visibility="hidden",
@@ -88,30 +97,41 @@ def main():
         )
 
         if user_answer:
-            script_template = PromptTemplate(
-                input_variables=["prompt"],
-                template="What is a good summary of the correct answer to: {prompt}?",
+            ai_message_prompt = AIMessagePromptTemplate.from_template(
+                interview_question
+            )
+            compare_text_template = ChatPromptTemplate.from_messages(
+                [
+                    human_message_prompt,
+                    ai_message_prompt,
+                    HumanMessagePromptTemplate.from_template(
+                        "Compare the the following answer to your own summary. Judge this answer and respond with a percentage that corresponds to your judgement. Here is the answer: {prompt1}"
+                    ),
+                ]
             )
 
-            compare_text_template = PromptTemplate(
-                input_variables=["prompt1", "prompt2"],
-                template=
-                    "Compare the user's answer to your own summary. Judge the user's answer and respond with a "
-                    "percentage that corresponds to your judgement. The user's answer was: {prompt1} and your "
-                    " own answer was: {prompt2}",
-                
+            compare_text = call_openai(
+                {"prompt": topic, "prompt1": user_answer}, compare_text_template
+            )
+            header.write(compare_text)
+
+            def rerun():
+                st.session_state.user_input = ""
+                st.session_state.user_answer = ""
+
+            st.button(
+                label="next topic",
+                on_click=rerun,
             )
 
-            with st.spinner("Loding"):
-                script = call_openai(interview_question, script_template)
-                compare_text = call_openai({'prompt1':"user_answer",'prompt2': "ai_answer"}, compare_text_template )
-                st.write(script)
-                st.write(compare_text)
-
-                input = {'topic':topic,'question':interview_question,'answer':user_answer,'aianswer':script}
-                database=insert_data_into_database(supabase, input=input)
-                print(database)
-        
+            input = {
+                "topic": topic,
+                "question": interview_question,
+                "answer": user_answer,
+                "aianswer": compare_text,
+            }
+            database = insert_data_into_database(supabase, input=input)
+            print(database)
 
 
 if __name__ == "__main__":
