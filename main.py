555f28899c29058bf228ec323b6adfda4acaf255
Amaly25 <116031938+Amaly25@users.noreply.github.com>
2023-08-02 14:54:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,137 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
-from langchain.schema import SystemMessage
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    AIMessagePromptTemplate
+)
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
+from supabase import create_client
 
 
-
-
-
+@st.cache_resource
 def init():
     load_dotenv()
+    url = os.environ.get("SUPABASE_URL")
+    key = os.environ.get("SUPABASE_KEY")
+    supabase = create_client(url, key)
+    # load the OpenAI API key from the enviroment variable
     if os.getenv("OPENAI_API_KEY") == "":
         print("OPENAI_API_KEY is not set")
         exit(1)
     else:
         print("OPENAI_API_KEY is get")
+    return supabase    
 
 
 @st.cache_data
 def call_openai(input, _template_name):
-    llm = OpenAI(temperature=0.9)
-    chain = LLMChain(llm=llm, prompt=_template_name)
-    response = chain.run(prompt=input)
+    model = ChatOpenAI(
+        model='gpt-3.5-turbo',
+        temperature=0,
+    )
+    chain = LLMChain(llm=model, prompt=_template_name)
+    response = chain.run(input)
     return response
 
+def get_data_base_data(supabase):
+    table = supabase.table("jobprep")
+    print('')
+    print('')
+    print('')
+    data = table.select("*").execute()
+    return data
+
+@st.cache_data
+def insert_data_into_database(_supabase, input: dict):
+    table= _supabase.table("jobprep")
+    insert_data= table.insert(input).execute()
+    return insert_data
 
 def main():
-    init()
+    supabase = init()
+    data = get_data_base_data(supabase)
+    print(data)
+
+
     header = st.container()
-    header.title("Job Interview Preparation..Your own knowledge Tester 🤖")
+    header.title("Job Prep Cards 🤖")
     header.subheader(
-        "Discover the ultimate job interview preparation app, designed to empower you with the skills and confidence needed to land the job of your dreams."
+        "Discover the ultimate job interview preparation app, designed "
+        "to empower you with the skills and confidence needed to land the job of your dreams."
     )
 
-    messages = [
-        SystemMessage(content=("You are a helpful assistant")),
-    ]
-
-    response = None
+    interview_question = None
     label = "Enter your topic here:"
-    prompt = header.text_input(
+    topic = header.text_input(
         label=label,
         key="user_input",
         label_visibility="hidden",
         placeholder=label,
     )
 
-    title_template = PromptTemplate(
-        input_variables=["prompt"],
-        template="write a an interview question about {prompt}",
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        "Generate a question about a specific topic, that could be asked in a job interview."
+        " ChatGPT should not ask for personal experiences etc. The topic is: {prompt}"
     )
+    topic_template = ChatPromptTemplate.from_messages([human_message_prompt])
 
-    if prompt:
+    if topic:
         with st.spinner("Generating a Question"):
-            response = call_openai(prompt, title_template)
-            st.write(response)
+            interview_question = call_openai(topic, topic_template)
+            header.write(interview_question)
 
-    if response:
-        label2 = "write your answer "
-        prompt1 = st.text_input(
-            label=label2,
+    if interview_question:
+        answer_label = "write your answer "
+        user_answer = header.text_input(
+            label=answer_label,
             key="user_answer",
             label_visibility="hidden",
-            placeholder=label2,
+            placeholder=answer_label,
         )
 
-        if prompt1:
-            script_template = PromptTemplate(
-                input_variables=["prompt"],
-                template="write me a appropriate answer based on this topic: {prompt} ",
+        if user_answer:
+            ai_message_prompt = AIMessagePromptTemplate.from_template(interview_question)
+            compare_text_template = ChatPromptTemplate.from_messages( 
+                [
+                    human_message_prompt,
+                    ai_message_prompt,
+                    HumanMessagePromptTemplate.from_template(
+                        "Compare the the following answer to your own summary. Judge this answer and respond with a percentage that corresponds to your judgement. Here is the answer: {prompt1}"
+                    ),
+
+            ]
+
             )
 
-            compare_text_template = PromptTemplate(
-                input_variables=["prompt"],
-                template="compare the user answer in the input prompt1 based on the script_templat AI Answer and give a percentaage of the correctness of the users answer: {prompt}",
-            )
+            
+            compare_text = call_openai({'prompt': topic, 'prompt1': user_answer},
+                                        compare_text_template)
+            header.write(compare_text)
 
-            with st.spinner("Loding"):
-                script = call_openai(response, script_template)
-                comper_text = call_openai(prompt1, compare_text_template)
-                st.write(script)
-                st.write(comper_text)
+            def rerun():
+                st.session_state.user_input = ''
+                st.session_state.user_answer = ''
+
+            st.button(
+                label='next topic',
+                on_click = rerun,
+            )
+           
+
+            input = {'topic':topic,'question':interview_question,'answer':user_answer,'aianswer': compare_text}
+            database=insert_data_into_database(supabase, input=input)
+            print(database)
+        
 
 
 if __name__ == "__main__":
     st.set_page_config(
-        page_title="Your own knowledge Tester",
+        page_title="Job Prep Cards",
         page_icon="🤖",
         layout="centered",
         initial_sidebar_state="expanded",
